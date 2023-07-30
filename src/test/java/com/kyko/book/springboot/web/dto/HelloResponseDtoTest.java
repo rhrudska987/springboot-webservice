a81d53dd60f7d7ca7d889800309e263d99fccced
rhrudska987 <rhrudska987@naver.com>
2023-07-30 18:25:28 +0900

    2023.07.30 tdd study

---
+++ src/test/java/com/kyko/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.kyko.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트( ){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
