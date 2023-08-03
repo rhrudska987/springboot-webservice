bd5286c885bdc9b617d1ea53b4c6b30c70de1063
rhrudska987 <rhrudska987@naver.com>
2023-08-03 18:17:55 +0900

    2023.08.03 make templates and modify controller and service

--- src/main/java/com/kyko/book/springboot/service/PostsService.java
+++ src/main/java/com/kyko/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kyko.book.springboot.service;
 
 import com.kyko.book.springboot.domain.posts.Posts;
 import com.kyko.book.springboot.domain.posts.PostsRepository;
+import com.kyko.book.springboot.web.dto.PostsListResponseDto;
 import com.kyko.book.springboot.web.dto.PostsResponseDto;
 import com.kyko.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kyko.book.springboot.web.dto.PostsUpdateReqestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +40,12 @@ public class PostsService {
         -> new IllegalArgumentException("핻당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
