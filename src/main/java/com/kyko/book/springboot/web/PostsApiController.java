81e19f54a417d31cc3e0c01418dbe4cec9a89191
rhrudska987 <rhrudska987@naver.com>
2023-08-05 19:32:00 +0900

    2023.08.03 make templates and modify controller and service2

--- src/main/java/com/kyko/book/springboot/web/PostsApiController.java
+++ src/main/java/com/kyko/book/springboot/web/PostsApiController.java
@@ -28,4 +28,10 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable("id") Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
 }
