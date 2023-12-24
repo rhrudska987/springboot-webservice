8110862f706bce59cbb4cb4eb3ff6730ea82c302
고경남 <rhrudska987@naver.com>
2023-12-24 18:48:47 +0900

    CI 추가

--- src/test/java/com/kyko/book/springboot/posts/PostsRepositoryTest.java
+++ src/test/java/com/kyko/book/springboot/posts/PostsRepositoryTest.java
@@ -1,19 +1,17 @@
 package com.kyko.book.springboot.posts;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.kyko.book.springboot.domain.posts.Posts;
 import com.kyko.book.springboot.domain.posts.PostsRepository;
-import org.assertj.core.api.Assertions;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
